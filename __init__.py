3e80bd2baedd3bc99e8858a9acf233058c02f802
Betti <akylu8@gmail.com>
2023-11-06 13:57:01 +0100

    added doc and bug report links

--- __init__.py
+++ __init__.py
@@ -22,9 +22,11 @@ bl_info = {
     "version": (2, 6),
     "blender": (2, 80, 0),
     "location": "View3D > Bone Merger",
-    "description": """Creates a chain of parenting between two bones, placing two intermediary empties""",
+    "description": """Creates a chain of parenting between two bones, placing two intermediary empties
+!!! DO NOT HESITATE TO REPORT ANY BUGS !!! Especially if the bug slows you down in the production PLZ PLZ PLZ REPORT""",
     "warning": "",
-    "doc_url": "",
+    "doc_url": "https://github.com/Betti83771/BlenderBoneMerger",
+    "tracker_url": "https://github.com/Betti83771/BlenderBoneMerger/issues",
     "category": "Rigging",
 }
 
